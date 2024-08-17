26e0afe56e3d9dcabf2bfe588b8aa75cd45caf7e
Matthew Brighton Smith <brightonsmith@gmail.com>
2024-08-17 14:44:23 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const body = document.getElementsByTagName("body")[0];
+const secondChild = body.querySelector("main:nth-child(1)");
+body.removeChild(secondChild);
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = ("Brighton Smith is the champion").toString();
+document.body.append(newHeader);
